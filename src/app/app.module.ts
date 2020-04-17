1f99d17df09c8078fd84e26451503933bcd17160
Muhammad Nuridin <muhammad.nuridin@rajamobil.net>
2020-04-17 12:28:35 +0700

    add form employee with modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { DataTablesModule } from 'angular-datatables';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { DataTablesModule } from 'angular-datatables';
 import { EmployeeComponent } from './employee/employee.component';
 import { LoginComponent } from './login/login.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 
 @NgModule({
   declarations: [
@@ -18,7 +22,11 @@ import { LoginComponent } from './login/login.component';
     BrowserModule,
     AppRoutingModule,
     DataTablesModule,
-    // NgbModule,
+    NgbModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    BsDatepickerModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import employee from '../_files/employee.json';
+import { NgbModal, ModalDismissReasons, NgbCalendar } from '@ng-bootstrap/ng-bootstrap';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-employee',
@@ -8,10 +10,71 @@ import employee from '../_files/employee.json';
 })
 export class EmployeeComponent implements OnInit {
   title = 'Employee';
+  formEmployee = new FormGroup({
+    username: new FormControl(),
+    firtsname: new FormControl(),
+    lastname: new FormControl(),
+    birthDate: new FormControl(),
+    basicSalary: new FormControl(),
+    status: new FormControl(),
+    description: new FormControl(),
+  });
+  closeResult: string;
   dtOptions: DataTables.Settings = {};
   employeeList = employee;
 
-  constructor() { }
+  constructor(
+    private modalService: NgbModal,
+    private calendar: NgbCalendar
+  ) { }
+
+  open(content: any) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+      this.formEmployee = new FormGroup({
+        username: new FormControl(),
+        firtsname: new FormControl(),
+        lastname: new FormControl(),
+        birthDate: new FormControl(),
+        basicSalary: new FormControl(),
+        status: new FormControl(),
+        description: new FormControl(),
+      });
+    }, (reason) => {
+      console.log('close', this.closeResult);
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
+
+  save(): void{
+    this.employeeList.push({
+      id: 3,
+      username: 'test',
+      firstName: 'test',
+      lastName: 'test',
+      email: 'test.tes@gmail.com',
+      birthDate: '1995-01-01',
+      basicSalary: '2000000',
+      status: 'single',
+      group: 'test',
+      description: '2020-04-15'
+    });
+    console.log('form', this.formEmployee.value);
+  }
+
+  selectToday() {
+    this.formEmployee.value.birthDate = this.calendar.getToday();
+  }
 
   ngOnInit(): void {
     this.dtOptions = {
